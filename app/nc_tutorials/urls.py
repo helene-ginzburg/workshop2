eb4e69674a85d03a5a118e3863136863c552e1dd
Helene Ginzburg <hginzburg@gmail.com>
2021-11-06 11:09:35 -0500

    urls.py corrected

--- app/nc_tutorials/urls.py
+++ app/nc_tutorials/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-from app.nc_tutorials.settings import STATIC_ROOT
+# from app.nc_tutorials.settings import STATIC_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
